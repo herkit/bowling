88a89f219018c5e22dfe7c84c208a140cbc2fb4e
herkit <henrik.grotle@arasoft.no>
2013-11-21 23:06:39 +0100

    set up solution

---
+++ src/BowlingScorer.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScorer.Tests
+{
+    public class Class1
+    {
+    }
+}
