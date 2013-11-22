ac924e9a3d56db655ba3049af7b95edac2e352e3
herkit <henrik.grotle@arasoft.no>
2013-11-22 01:26:27 +0100

    split frame tests from scoring

---
+++ src/BowlingScorer.Tests/FrameTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace BowlingScorer.Tests
+{
+    [TestFixture]
+    public class FrameTests
+    {
+        [Test]
+        public void Should_be_able_to_query_frame_if_it_is_a_strike()
+        {
+            var frame = new Frame { Rolls = new[] { 10 } };
+            frame.IsStrike.ShouldEqual(true);
+        }
+
+        [Test]
+        public void Should_be_able_to_query_frame_if_it_is_a_spare()
+        {
+            var frame = new Frame { Rolls = new[] { 5, 5 } };
+            frame.IsStrike.ShouldEqual(false);
+            frame.IsSpare.ShouldEqual(true);
+        }
+
+        [Test]
+        public void Should_be_able_to_identify_spares_in_last_frame()
+        {
+            var frame = new Frame { Rolls = new[] { 4, 6, 2 } };
+            frame.IsStrike.ShouldEqual(false);
+            frame.IsSpare.ShouldEqual(true);
+        }
+
+
+        [Test]
+        public void ToString_should_return_string_representation_of_frame()
+        {
+            new Frame { Rolls = new[] { 5, 5 } }.ToString().ShouldEqual("5 /");
+            new Frame { Rolls = new[] { 10 } }.ToString().ShouldEqual(" X ");
+            new Frame { Rolls = new[] { 0, 3 } }.ToString().ShouldEqual("0 3");
+            new Frame { Rolls = new[] { 5, 5, 2 } }.ToString().ShouldEqual("5/2");
+            new Frame { Rolls = new[] { 10, 10, 10 } }.ToString().ShouldEqual("XXX");
+            new Frame { Rolls = new[] { 10, 10 } }.ToString().ShouldEqual("XX ");
+        }
+    }
+}
--- src/BowlingScorer.Tests/ScorerTests.cs
+++ src/BowlingScorer.Tests/ScorerTests.cs
@@ -85,39 +85,5 @@ namespace BowlingScorer.Tests
             frames[9].Rolls.ShouldEqual(new [] { 10, 5, 5} );
         }
 
-        [Test]
-        public void Should_be_able_to_query_frame_if_it_is_a_strike()
-        {
-            var frame = new Frame {Rolls = new[] { 10 } };
-            frame.IsStrike.ShouldEqual(true);
-        }
-
-        [Test]
-        public void Should_be_able_to_query_frame_if_it_is_a_spare()
-        {
-            var frame = new Frame { Rolls = new[] { 5, 5 } };
-            frame.IsStrike.ShouldEqual(false);
-            frame.IsSpare.ShouldEqual(true);            
-        }
-
-        [Test]
-        public void Should_be_able_to_identify_spares_in_last_frame()
-        {
-            var frame = new Frame { Rolls = new[] { 4, 6, 2 } };
-            frame.IsStrike.ShouldEqual(false);
-            frame.IsSpare.ShouldEqual(true);
-        }
-
-
-        [Test]
-        public void ToString_should_return_string_representation_of_frame()
-        {
-            new Frame { Rolls = new [] { 5, 5 } }.ToString().ShouldEqual("5 /");
-            new Frame { Rolls = new [] { 10 } }.ToString().ShouldEqual(" X ");
-            new Frame { Rolls = new [] { 0, 3 } }.ToString().ShouldEqual("0 3");
-            new Frame { Rolls = new [] { 5, 5, 2 } }.ToString().ShouldEqual("5/2");
-            new Frame { Rolls = new[] { 10, 10, 10 } }.ToString().ShouldEqual("XXX");
-        }
-
     }
 }
