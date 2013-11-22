c6c4a2ad190a2d4fcafefcd517a6a9da3e1dbf1e
herkit <henrik.grotle@arasoft.no>
2013-11-22 01:47:00 +0100

    removed redundant else statements in Frame.ToString

--- src/BowlingScorer/Frame.cs
+++ src/BowlingScorer/Frame.cs
@@ -35,21 +35,17 @@ namespace BowlingScorer
                 output += Rolls[2] == 10 ? "X" : Rolls.Skip(1).Take(2).Sum() == 10 ? "/" : Rolls[2].ToString();
                 return output;
             }
-            else
+
+            if (IsSpare)
+                return String.Format("{0} /", Rolls[0]);
+                
+            if (IsStrike)
             {
-                if (IsSpare)
-                    return String.Format("{0} /", Rolls[0]);
-                else if (IsStrike)
-                {
-                    if (Rolls.Length == 1)
-                        return String.Format(" X ");
-                    else
-                    {
-                        return "X" + (Rolls[1] == 10 ? "X" : Rolls[1].ToString()) + " ";
-                    }
-                         
-                }
+                if (Rolls.Length == 1)
+                    return String.Format(" X ");
+                return "X" + (Rolls[1] == 10 ? "X" : Rolls[1].ToString()) + " ";
             }
+
             return String.Join(" ", Rolls) + (Rolls.Length == 1 ? "  " : "");
         }
     }
